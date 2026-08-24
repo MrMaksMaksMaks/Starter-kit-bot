a506d339f43a5450cf2ce599ddb6243e7a3feeec
Maksim Gorbunov <mrmaksmaksmaks@gmail.com>
2026-08-24 15:40:35 +0300

    Initial commit: Starter-Kit-Bot project structure

---
+++ programs/starter-kit-bot/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("E8So5T6TkzUN5vu8NXC4r1XfFEBXjsTiEV2rMUcYdPaF");
+
+#[program]
+pub mod starter_kit_bot {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        crate::instructions::initialize::handle_initialize(ctx)
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        crate::instructions::increment::handle_increment(ctx)
+    }
+}
